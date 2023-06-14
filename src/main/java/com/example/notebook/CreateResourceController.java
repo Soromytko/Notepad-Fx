da2308edf89d7a5092f5fbf375929259b00dd090
Soromytko <NikitaSoromytko@gmail.com>
2023-06-15 02:39:06 +0300

    add backend (create_resource)

--- src/main/java/com/example/notebook/CreateResourceController.java
+++ src/main/java/com/example/notebook/CreateResourceController.java
@@ -1,32 +1,52 @@
 package com.example.notebook;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.net.URL;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
-public class CreateResourceController {
-    @FXML
-    private void onAddCategory() {
-        Parent root;
-        try {
-            root = FXMLLoader.load(HelloApplication.class.getResource("add_category.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("My New Stage Title");
-            stage.setScene(new Scene(root));
-            stage.show();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+public class CreateResourceController implements Initializable {
+    @FXML private TextField resourceName;
+    @FXML private TextField resourceUrl;
+    @FXML private ComboBox comboBox;
+
+    public CreateResourceController() {
+
     }
 
-    @FXML
-    private void onAddResource() {
-	    System.out.println("on Click");
+    @Override public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
+        comboBox.setItems(langs);
+        comboBox.setValue("Java");
     }
 
+    @FXML private void onAddCategory(ActionEvent event) {
+        Stage stage = WindowLoader.load("add_category.fxml");
+    }
+
+    @FXML private void onAddResource(ActionEvent event) {
+
+        System.out.println(resourceName.getText());
+        System.out.println(resourceUrl.getText());
+
+
+
+        return;
+
+//	    System.out.println("on Click");
+//
+//        Stage thisStage = (Stage) WindowLoader.getScene(event).getWindow();
+//        thisStage.close();
+    }
 }
--- src/main/java/com/example/notebook/WindowLoader.java
+++ src/main/java/com/example/notebook/WindowLoader.java
@@ -1,9 +1,12 @@
 package com.example.notebook;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
@@ -23,5 +26,11 @@ public class WindowLoader {
         }
 
         return null;
+    }
+
+    public static Scene getScene(ActionEvent event) {
+        //        ((Node)(event.getSource())).getScene().getWindow().hide();
+        Node node = (Node)(event.getSource());
+        return node.getScene();
     }
 }
